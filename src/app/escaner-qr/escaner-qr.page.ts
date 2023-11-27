f0b34680b05b26d1e544ad1d0fc0a9fde7daa655
JawiniSKP <127449669+JawiniSKP@users.noreply.github.com>
2023-11-27 00:00:02 -0300

    help

--- src/app/escaner-qr/escaner-qr.page.ts
+++ src/app/escaner-qr/escaner-qr.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-escaner-qr',
   templateUrl: './escaner-qr.page.html',
   styleUrls: ['./escaner-qr.page.scss'],
 })
-export class EscanerQRPage implements OnInit {
+export class EscanerQRPage implements AfterViewInit {
+  @ViewChild('videoElement') video!: ElementRef<HTMLVideoElement>;
 
-  constructor() { }
-
-  ngOnInit() {
+  // Implementa el método ngAfterViewInit
+  ngAfterViewInit(): void {
+   this.prepareScanner()
+   
   }
 
+  async prepareScanner() {
+    const avaible = this.chackCamera()
+  }
+
+  async chackCamera() {
+    const cameraPermissions = await navigator.permissions.query({name:'camera'} as any)  
+    console.log(cameraPermissions)
+  }
 }
